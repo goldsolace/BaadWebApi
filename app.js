f1126a348c6c997cbb7cfbbb47f67b9d4c2bdb4a
goldsolace <goldsolace@gmail.com>
2019-04-24 02:45:36 +1000

    Fixed names

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname);
-console.log(__dirname + '/public');
+app.use('/public', express.static('public'));
 
 
 app.use('/node_modules',express.static(__dirname +'/node_modules'));
