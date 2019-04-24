527c857d2cf441b60232a0713f4574855b272ed2
goldsolace <goldsolace@gmail.com>
2019-04-24 19:00:18 +1000

    Landing page

--- public/angular-app/landing/landing-controller.js
+++ public/angular-app/landing/landing-controller.js
@@ -2,8 +2,11 @@ angular.module('baadwebapi').controller('LandingController', LandingController);
 function LandingController(landingDataFactory) {
 	var vm = this;
 	vm.title = 'Baad Scripts';
-	landingDataFactory.landing().then(function (response) {
-		if (response)
-			vm.landing = response.data;
-	});
+	vm.landing = {
+		test : "test"
+	};
+// 	landingDataFactory.landing().then(function (response) {
+// 		if (response)
+// 			vm.landing = response.data;
+// 	});
 }
